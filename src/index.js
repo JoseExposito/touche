6fc859021ad5eb756abf01576b9333492a642afe
JoseExposito <jose.exposito89@gmail.com>
2020-10-03 18:57:24 +0200

    Add GTK hello world example

--- src/index.js
+++ src/index.js
@@ -41,3 +41,70 @@ log(JSON.stringify(store.getState()));
 
 const result = sum5(10);
 log(result);
+
+// -------------------------------------------------------------------------------------------------
+
+imports.gi.versions.Gtk = '3.0';
+const { Gio, Gtk } = imports.gi;
+
+class ImageViewerWindow {
+  constructor(app) {
+    this.app = app;
+    this.window = null;
+    this.box = null;
+    this.image = null;
+    this.fileChooserButton = null;
+  }
+
+  buildUI() {
+    this.window = new Gtk.ApplicationWindow({
+      application: this.app,
+      defaultHeight: 600,
+      defaultWidth: 800,
+    });
+    this.box = new Gtk.Box({
+      orientation: Gtk.Orientation.VERTICAL,
+    });
+
+    this.image = new Gtk.Image({
+      vexpand: true,
+    });
+    this.box.add(this.image);
+
+    this.fileChooserButton = Gtk.FileChooserButton.new('Pick An Image', Gtk.FileChooserAction.OPEN);
+
+    this.fileChooserButton.connect('file-set', () => {
+      // const fileName = button.get_filename();
+      const fileName = this.fileChooserButton.get_filename();
+      this.image.set_from_file(fileName);
+    });
+
+    this.box.add(this.fileChooserButton);
+    this.box.show_all();
+
+    this.window.add(this.box);
+  }
+
+  getWidget() {
+    this.buildUI();
+    return this.window;
+  }
+}
+
+const application = new Gtk.Application({
+  application_id: 'org.gnome.Sandbox.ImageViewerExample',
+  flags: Gio.ApplicationFlags.FLAGS_NONE,
+});
+
+application.connect('activate', (app) => {
+  let { activeWindow } = app;
+
+  if (!activeWindow) {
+    const imageViewerWindow = new ImageViewerWindow(app);
+    activeWindow = imageViewerWindow.getWidget();
+  }
+
+  activeWindow.present();
+});
+
+application.run(null);
