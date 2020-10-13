4722c2e9d0625f7926a971dedb4afef33e420926
JoseExposito <jose.exposito89@gmail.com>
2020-10-13 19:26:58 +0200

    Upgrade to webpack v5

--- bundle/webpack.config.js
+++ bundle/webpack.config.js
@@ -177,16 +177,4 @@ module.exports = {
       },
     ],
   },
-
-  // Some libraries import Node modules but don't use them in the browser.
-  // Tell Webpack to provide empty mocks for them so importing them works.
-  node: {
-    module: 'empty',
-    dgram: 'empty',
-    dns: 'mock',
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-    child_process: 'empty',
-  },
 };
