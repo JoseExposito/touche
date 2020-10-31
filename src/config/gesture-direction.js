a7559b0d2f47c4ca59599e79733a2bafffb0ea3b
JoseExposito <jose.exposito89@gmail.com>
2020-10-31 22:10:49 +0100

    Add utils to parse the config

---
+++ src/config/gesture-direction.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2020 José Expósito <jose.exposito89@gmail.com>
+ *
+ * This file is part of Touchégg-GUI.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation,  either version 3 of the License,  or (at your option)  any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful,  but  WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the  GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+const GestureDirection = {
+  // GestureType.SWIPE
+  UP: 'UP',
+  DOWN: 'DOWN',
+  LEFT: 'LEFT',
+  RIGHT: 'RIGHT',
+
+  // GestureType.PINCH
+  IN: 'IN',
+  OUT: 'OUT',
+};
+
+export default GestureDirection;
---
+++ src/config/io.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2020 José Expósito <jose.exposito89@gmail.com>
+ *
+ * This file is part of Touchégg-GUI.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation,  either version 3 of the License,  or (at your option)  any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful,  but  WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the  GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+const { GLib, Gio } = imports.gi;
+
+/**
+ * @returns {string} The user's configuration file path.
+ */
+export const getUserConfigPath = () => (
+  GLib.build_filenamev([GLib.get_home_dir(), '.config', 'touchegg', 'touchegg.conf'])
+);
+
+/**
+ * @returns {string} The user's configuration file content.
+ */
+export const getUserConfig = () => new Promise((resolve) => {
+  const path = getUserConfigPath();
+  const file = Gio.File.new_for_path(path);
+  file.load_contents_async(null, (fileCallback, result) => {
+    const contents = file.load_contents_finish(result)[1].toString();
+    resolve(contents);
+  });
+});
